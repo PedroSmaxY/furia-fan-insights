1b665e25ec8c6aa70c24f8c3edf74101346aa630
PedroSmaxY <pedrohnovais@outlook.com>
2025-04-30 19:46:35 -0300

    adicionando suporte para criação de banco de dados com contexto assíncrono

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,19 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.core.db import create_db_and_tables
 from app.api.main import api_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(title="Furia Fan Insights API", version="0.1.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
